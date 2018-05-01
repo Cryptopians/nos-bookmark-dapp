c1dd42add2238f2fc29c7dabd171a0df60e67a1e
Rob Moorman <rob@moori.nl>
2018-05-01 18:23:39 +0200

    Abstract methods for SC

--- contracts/nos_bookmark.py
+++ contracts/nos_bookmark.py
@@ -1,4 +1,5 @@
 from boa.builtins import concat
+from boa.code.builtins import list
 from boa.interop.Neo.Runtime import CheckWitness, GetTrigger, Log
 from boa.interop.Neo.Storage import Delete, Get, GetContext, Put
 from boa.interop.Neo.TriggerType import Application, Verification
@@ -12,4 +13,37 @@ def Main(operation, args):
     :param args: list A list of arguments along with the operation
     :return: bytearray: The result of the operation
     """
+    # TODO: Carefully read http://neo-boa.readthedocs.io/en/latest/overview.html#what-parts-of-python-are-supported
+    address_from = args[0]
+
+    # Verify if the address we want to operate from is the invoking address
+    is_owner = CheckWitness(address_from)
+
+    if not is_owner:
+        Log("Must be the owner of the address")
+        return False
+
+    # Crud operations
+    if operation == 'AddBookmark':
+        return True
+
+    if operation == 'SetBookmark':
+        return True
+
+    if operation == 'DeleteBookmark':
+        return True
+
+    # Share operations
+    if operations == 'ShareBookmark':
+        return True
+
+    if operation == 'ShareAllBookmarks':
+        return True
+
+    if operation == 'ApproveBookmark':
+        return True
+
+    if operation == 'RejectBookmark':
+        return True
+
     return False
