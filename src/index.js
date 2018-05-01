b22883e811238fdde5038f88f30f19083182afc0
Rob Moorman <rob@moori.nl>
2018-05-01 14:06:18 +0200

    Add initial build pipeline

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Index = () => {
+  return <div>nOS SSO dApp</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById("nos-dapp"))
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      title: 'nOS SSO dApp'
+    })
+  ]
+};
